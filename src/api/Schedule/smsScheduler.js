e732faf2d6cdb5b5b7a23fbf47683a561c73df11
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-07-03 14:39:31 +0530

    forgot password updation

--- src/api/Schedule/smsScheduler.js
+++ src/api/Schedule/smsScheduler.js
@@ -1,33 +1,33 @@
-const cron = require('node-cron');
-const { sendSMS } = require('../services/smsService.js');
-const Student = require("../models/student.model.js");
-const Institute = require("../models/institute.model.js");
+// const cron = require('node-cron');
+// const { sendSMS } = require('../services/smsService.js');
+// const Student = require("../models/student.model.js");
+// const Institute = require("../models/institute.model.js");
 
-const smsScheduler = async () => {
-  try {
-    // Fetch all institutes with notifyParents set to true
-    const institutes = await Institute.find({ notifyParents: true });
+// const smsScheduler = async () => {
+//   try {
+//     // Fetch all institutes with notifyParents set to true
+//     const institutes = await Institute.find({ notifyParents: true });
 
-    institutes.forEach(institute => {
-      const { sendTime } = institute.smsPreferences;
-      const [hour, minute] = sendTime.split(':');
+//     institutes.forEach(institute => {
+//       const { sendTime } = institute.smsPreferences;
+//       const [hour, minute] = sendTime.split(':');
 
-      // Schedule SMS based on each institute's preferences
-      cron.schedule(`${minute} ${hour} * * *`, async () => {
-        // Fetch all students of this institute with notifyParents set to true
-        const students = await Student.find({ institute: institute._id, notifyParents: true });
+//       // Schedule SMS based on each institute's preferences
+//       cron.schedule(`${minute} ${hour} * * *`, async () => {
+//         // Fetch all students of this institute with notifyParents set to true
+//         const students = await Student.find({ institute: institute._id, notifyParents: true });
 
-        students.forEach(async student => {
-          const message = `Your son/daughter ${student.name} is absent today. Please take necessary action.`;
-          await sendSMS(student.parentNumber, message);
-        });
+//         students.forEach(async student => {
+//           const message = `Your son/daughter ${student.name} is absent today. Please take necessary action.`;
+//           await sendSMS(student.parentNumber, message);
+//         });
 
-        console.log(`SMS notifications sent to parents of institute ${institute.name}`);
-      });
-    });
-  } catch (error) {
-    console.error('Error scheduling SMS notifications:', error);
-  }
-};
+//         console.log(`SMS notifications sent to parents of institute ${institute.name}`);
+//       });
+//     });
+//   } catch (error) {
+//     console.error('Error scheduling SMS notifications:', error);
+//   }
+// };
 
-module.exports = smsScheduler;
+// module.exports = smsScheduler;
--- src/api/Services/smsService.js
+++ src/api/Services/smsService.js
@@ -1,22 +1,22 @@
-const twilio = require('twilio');
+// const twilio = require('twilio');
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
-const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
-const fromPhoneNumber = process.env.TWILIO_PHONE_NUMBER; // Your Twilio phone number
+// const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+// const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
+// const fromPhoneNumber = process.env.TWILIO_PHONE_NUMBER; // Your Twilio phone number
 
-const client = new twilio(accountSid, authToken);
+// const client = new twilio(accountSid, authToken);
 
-const sendSMS = async (to, message) => {
-  try {
-    const response = await client.messages.create({
-      body: message,
-      to, // Text this number
-      from: fromPhoneNumber // From a valid Twilio number
-    });
-    console.log('SMS sent successfully:', response.sid);
-  } catch (error) {
-    console.error('Error sending SMS:', error);
-  }
-};
+// const sendSMS = async (to, message) => {
+//   try {
+//     const response = await client.messages.create({
+//       body: message,
+//       to, // Text this number
+//       from: fromPhoneNumber // From a valid Twilio number
+//     });
+//     console.log('SMS sent successfully:', response.sid);
+//   } catch (error) {
+//     console.error('Error sending SMS:', error);
+//   }
+// };
 
-module.exports = { sendSMS };
+// module.exports = { sendSMS };
